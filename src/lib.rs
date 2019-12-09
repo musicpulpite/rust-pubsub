f53be86cef03b74a10675157905894e7bc32fae7
William McAdams <musicpulpite@gmail.com>
2019-12-08 19:49:03 -0500

    Temp commit before refactor ws client handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,11 @@
 extern crate ws;
-use ws::{Handler, Sender, util::Token};
+use ws::{Handler, Sender, Message};
 
 use std::collections::{BTreeSet, HashMap};
 
-pub enum PubSubMessage {
-    // For now I won't worry about subbing/unsubbing to an array of channels
-    SUBSCRIBE { channel: String },
-    UNSUBSCRIBE { channel: String },
-    PUBLISH { channel: String, msg: String},
-    PSUBSCRIBE { pattern: String },
-    PUNSUBSCRIBE { pattern: String},
-}
-
-pub fn parse_message(msg: String) -> Result<PubSubMessage, String> {
-    let msg_contents: Vec<String> = msg.split(" ").collect();
-
-    return match msg_contents.as_slice() {
-        ["SUBSCRBE", channel] => Ok(PubSubMessage::SUBSCRIBE { channel }),
-        ["UNSUBSCRIBE", channel] => Ok(PubSubMessage::UNSUBSCRIBE { channel }),
-        ["PUBLISH", channel, msg] => Ok(PubSubMessage::PUBLISH { channel, msg }),
-        _ => Err("Could not parse ws message.")
-
-    }
-}
-
 pub struct Server {
+    // This clients map should be temporary, eventually the clients (Sender struct)
+    // will just be referenced directly from the channels
     pub clients: HashMap<Token, Sender>,
     pub channels: HashMap<String, BTreeSet<Token>>
 }
@@ -44,7 +25,7 @@ impl Server {
 
     pub fn sub_client(&mut self, client_token: Token, channel: String) -> Result<(), String> {
         // Cloning the channel string here for now so I can use it below, will come back and reassess
-        let mut subbed_clients = self.channels.entry(channel.clone()).or_insert(BTreeSet::new());
+        let subbed_clients = self.channels.entry(channel.clone()).or_insert(BTreeSet::new());
         let result = subbed_clients.insert(client_token);
 
         return if result {
@@ -52,5 +33,54 @@ impl Server {
         } else {
             Err(format!("Client {:?} already subscribed to channel {}.", client_token, channel))
         }
+    }
+
+    pub fn unsub_client(&mut self, client_token: Token, channel: String) -> Result<(), String> {
+        // Illuminating distinction about how Rust works, the methods above needed owned vars - these need refs.
+        if let Some(subbed_clients) = self.channels.get_mut(&channel) {
+            subbed_clients.remove(&client_token);
+            return Ok(())
+        } else {
+            return Err(format!("Client {:?} was never subscribed to channel {}.", client_token, channel))
+        }
+    }
+}
+
+pub struct SenderHandle {
+    sender: Sender
+}
+
+pub enum PubSubMessage {
+    // For now I won't worry about subbing/unsubbing to an array of channels
+    SUBSCRIBE { channel: String },
+    UNSUBSCRIBE { channel: String },
+    PUBLISH { channel: String, msg: String},
+    PSUBSCRIBE { pattern: String },
+    PUNSUBSCRIBE { pattern: String},
+}
+
+impl SenderHandle {
+    fn parse_message(&self, msg: String) -> Result<PubSubMessage, String> {
+        let msg_contents: Vec<&str> = msg.split(" ").collect();
+    
+        return match msg_contents.as_slice() {
+            ["SUBSCRIBE", channel] => Ok(PubSubMessage::SUBSCRIBE { channel: channel.to_string() }),
+            ["UNSUBSCRIBE", channel] => Ok(PubSubMessage::UNSUBSCRIBE { channel: channel.to_string() }),
+            ["PUBLISH", channel, msg] => Ok(PubSubMessage::PUBLISH { channel: channel.to_string(), msg: msg.to_string() }),
+            _ => Err("Could not parse ws message.".to_string())
+        }
+    }
+}
+
+impl Handler for SenderHandle {
+    fn on_message(&mut self, msg: Message) -> Result<(), ws::Err> {
+        match self.parse_message(msg.into_text().unwrap()).unwrap() {
+            PubSubMessage::SUBSCRIBE { channel } => ws_server.sub_client(client_token, channel).unwrap(),
+            PubSubMessage::UNSUBSCRIBE { channel } => ws_server.unsub_client(client_token, channel).unwrap(),
+            PubSubMessage::PUBLISH { channel, msg } => println!("Publishing {} on channel {}", msg, channel),
+            _ => Err("Some other WS message".to_string())
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate ws;
-use ws::listen;
+use ws::{ listen, Message };
 
 use std::{env, process};
 
 use rust_pubsub::Server;
+use rust_pubsub::{ parse_message, PubSubMessage };
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,16 +13,21 @@ fn main() {
     let mut ws_server = Server::new();
 
     if let Err(error) = listen(format!("127.0.0.1:{}", port), |client| { 
-        let client_token = client.token().clone();
+        let client_token = client.token().clone(); // Probably temporary as well
         ws_server.add_client(client);
 
-        move |msg| {
+        move |msg: Message| {
             println!("Got message from client {:?}.", client_token);
             println!("{}", msg);
 
-            return match parse_message(msg) {
-
+            match parse_message(msg.into_text().unwrap()).unwrap() {
+                PubSubMessage::SUBSCRIBE { channel } => ws_server.sub_client(client_token, channel).unwrap(),
+                PubSubMessage::UNSUBSCRIBE { channel } => ws_server.unsub_client(client_token, channel).unwrap(),
+                PubSubMessage::PUBLISH { channel, msg } => println!("Publishing {} on channel {}", msg, channel),
+                _ => println!("Some other WS message")
             }
+
+            Ok(())
         }
     }) {
         println!("Failed to start WebSocket server on port {}.", port);
