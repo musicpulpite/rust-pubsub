72dcd5e025f959ca4c19b9ae1fe3f8c065da7a01
William McAdams <musicpulpite@gmail.com>
2019-12-25 15:03:22 -0500

    Add message publishing logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
 extern crate ws;
-use ws::{ Handshake, Handler, Sender, Message, Error, ErrorKind, util::Token };
+use ws::{ 
+    Handshake,
+    Handler,
+    Sender,
+    Message,
+    Error,
+    ErrorKind,
+    CloseCode,
+    util::Token
+};
 
 use std::collections::{ BTreeSet, HashMap };
 use std::rc::Rc;
@@ -19,13 +28,21 @@ impl Server {
         }
     }
 
-    pub fn add_client(&mut self, client: &Sender) -> () {
+    pub fn add_client(&mut self, client: &Sender) {
         let client_token = client.token().clone();
         self.clients.insert(client_token, client.clone());
     }
 
+    pub fn remove_client(&mut self, token: &Token) {
+        self.clients.remove(token);
+        // would it be more efficient to leave these tokens in each channel and remove them
+        // on the next pub message once we see that the client has been removed?
+        for subbed_clients in self.channels.values_mut() {
+            subbed_clients.remove(token);
+        }
+    }
+
     pub fn sub_client(&mut self, client: &Token, channel: String) -> Result<(), Error> {
-        // Cloning the channel string here for now so I can use it below, will come back and reassess
         let subbed_clients = self.channels.entry(channel.clone()).or_insert(BTreeSet::new());
         let result = subbed_clients.insert(client.clone());
 
@@ -33,7 +50,6 @@ impl Server {
             Ok(())
         } else {
             Err(Error { kind: ErrorKind::Internal, details: Cow::Borrowed("The client was already subscribed to this channel.") })
-            // Err(format!("Client {:?} already subscribed to channel {}.", client, channel))
         }
     }
 
@@ -44,7 +60,21 @@ impl Server {
             return Ok(())
         } else {
             Err(Error { kind: ErrorKind::Internal, details: Cow::Borrowed("The client was never subscribed to this channel.") })
-            // return Err(format!("Client {:?} was never subscribed to channel {}.", client, channel))
+        }
+    }
+
+    pub fn pub_message(&mut self, channel: String, msg: String ) -> Result<(), Error> {
+        // Probably bad practice to pass an owned variable into the function but use its reference, will come back
+        if let Some(subbed_clients) = self.channels.get_mut(&channel) {
+            for client_token in subbed_clients.iter() {
+                if let Some(client) = self.clients.get(client_token) {
+                    client.send(msg.clone())?;
+                }
+            }
+
+            Ok(())
+        } else {
+            Ok(())
         }
     }
 }
@@ -59,19 +89,19 @@ pub enum PubSubMessage {
     SUBSCRIBE { channel: String },
     UNSUBSCRIBE { channel: String },
     PUBLISH { channel: String, msg: String},
-    PSUBSCRIBE { pattern: String },
-    PUNSUBSCRIBE { pattern: String},
+    // PSUBSCRIBE { pattern: String },
+    // PUNSUBSCRIBE { pattern: String},
 }
 
 impl ClientHandle {
-    fn parse_message(&self, msg: String) -> Result<PubSubMessage, String> {
+    fn parse_message(&self, msg: String) -> Result<PubSubMessage, Error> {
         let msg_contents: Vec<&str> = msg.split(" ").collect();
     
         return match msg_contents.as_slice() {
             ["SUBSCRIBE", channel] => Ok(PubSubMessage::SUBSCRIBE { channel: channel.to_string() }),
             ["UNSUBSCRIBE", channel] => Ok(PubSubMessage::UNSUBSCRIBE { channel: channel.to_string() }),
             ["PUBLISH", channel, msg] => Ok(PubSubMessage::PUBLISH { channel: channel.to_string(), msg: msg.to_string() }),
-            _ => Err("Could not parse ws message.".to_string())
+            _ => Err(Error { kind: ErrorKind::Protocol, details: Cow::Borrowed("Received invalid WS message.") })
         }
     }
 }
@@ -81,14 +111,16 @@ impl Handler for ClientHandle {
         self.ws_server_ref.borrow_mut().add_client(&self.client);
         Ok(())
     }
+
+    fn on_close(&mut self, _: CloseCode, _: &str) {
+        self.ws_server_ref.borrow_mut().remove_client(&self.client.token());
+    }
     
     fn on_message(&mut self, msg: Message) -> Result<(), Error> {
-        return match self.parse_message(msg.into_text().unwrap()).unwrap() {
-            // Probably going to need to use Rc's here as well since BTreeSet::insert needs to take ownership
+        return match self.parse_message(msg.into_text().unwrap())? {
             PubSubMessage::SUBSCRIBE { channel } => self.ws_server_ref.borrow_mut().sub_client(&self.client.token(), channel),
             PubSubMessage::UNSUBSCRIBE { channel } => self.ws_server_ref.borrow_mut().unsub_client(&self.client.token(), channel),
-            PubSubMessage::PUBLISH { channel, msg } => Ok(()),
-            _ => Err(Error { kind: ErrorKind::Protocol, details: Cow::Borrowed("Some other WS message") })
+            PubSubMessage::PUBLISH { channel, msg } => self.ws_server_ref.borrow_mut().pub_message(channel, msg),
         }
     }
 }
